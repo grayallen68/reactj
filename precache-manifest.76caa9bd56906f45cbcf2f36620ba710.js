b42b8306bf2d4bbd8d34d62e56d7017f259012fc
grayallen68 <graysonewing3b@gmail.com>
2019-04-26 13:47:11 -0600

    Updates

---
+++ precache-manifest.76caa9bd56906f45cbcf2f36620ba710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d3a62db349ea3cbf55d2c98e7963e2",
+    "url": "/reactj/static/media/blackbird.a8d3a62d.png"
+  },
+  {
+    "revision": "7e6e1860da118a93d8e7",
+    "url": "/reactj/static/js/runtime~main.ea8efec5.js"
+  },
+  {
+    "revision": "4d32b9cc5c1de0590a66",
+    "url": "/reactj/static/js/main.50d032bb.chunk.js"
+  },
+  {
+    "revision": "e8138f7eb02f4795b510",
+    "url": "/reactj/static/js/2.2d9eb69d.chunk.js"
+  },
+  {
+    "revision": "4d32b9cc5c1de0590a66",
+    "url": "/reactj/static/css/main.0e4c918b.chunk.css"
+  },
+  {
+    "revision": "e8138f7eb02f4795b510",
+    "url": "/reactj/static/css/2.f873262d.chunk.css"
+  },
+  {
+    "revision": "c682fe07136cb935471695d4e6c22c82",
+    "url": "/reactj/index.html"
+  }
+];
